d4673ea0b786d5192666bedd98fd813ca02f8a18
kiridharan <kiridharan@gmail.com>
2021-08-17 23:07:04 +0530

    started and created branch

--- src/auth.py
+++ src/auth.py
@@ -1,1 +1,11 @@
-import flask
+from flask import Flask
+
+
+
+app = Flask(__name__)
+users = [{'uid': 1, 'name': 'Noah Schairer'}]
+@app.route('/api/userinfo')
+def userinfo():
+    return {'data': users}, 200
+if __name__ == '__main__':
+    app.run(debug=True)
